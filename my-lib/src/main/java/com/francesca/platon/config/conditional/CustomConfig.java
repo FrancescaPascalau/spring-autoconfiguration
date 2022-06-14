b4a1ce5fcb798f15485a5618387093f95d8ccaa3
FrancescaPascalau <pascalaufrancescaa@gmail.com>
2022-06-14 08:53:07 +0200

    Fixed path to bean.

--- my-lib/src/main/java/com/francesca/platon/config/conditional/CustomConfig.java
+++ my-lib/src/main/java/com/francesca/platon/config/conditional/CustomConfig.java
@@ -3,7 +3,7 @@ package com.francesca.platon.config.conditional;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
-@ConditionalOnMissingBean(type = "com.francesca.platon.config.MyBean")
+@ConditionalOnMissingBean(type = "com.francesca.platon.app.config.MyBean")
 public class CustomConfig {
 
     @Bean("conditionalMessage")
